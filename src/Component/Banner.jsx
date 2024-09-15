1d652d5f75530f490a46d324ee288efb6c0d0281
shovonkhan200h <shovonkhan00@gmail.com>
2024-09-15 05:17:21 +0100

    update

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -8,8 +8,6 @@ const Banner = () => {
   return (
     <div id="home" className="relative w-full min-h-screen bg-gradient-to-r from-rose-500 to-blue-500 items-center justify-center flex gap-4 p-5">
       
-      
-
       <div className="w-full">
       <BannerContent/>
       </div>
@@ -21,7 +19,7 @@ const Banner = () => {
       <div className="h-[50px] bg-black absolute inset-x-0 -bottom-5 flex justify-evenly items-center">
         {
           headerItem.map((item,id)=>{
-            return <div className="flex items-center gap-2 font-semibold justify-center text-white text-sm lg:text-lg" key={id}>
+            return <div className="flex px-2 items-center gap-2 font-semibold justify-center text-white text-[12px] md:text-sm lg:text-lg" key={id}>
               <a>{item.icon}</a>
               <h>{item.text}</h>
             </div>
